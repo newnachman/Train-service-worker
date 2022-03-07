5f2f46c199f0dd0cbd7239ead76c878ca022e417
nachman alush <newnachman@gmail.com>
2022-03-07 12:54:22 +0200

    added explanation for main.js file

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ if ('serviceWorker' in navigator) {
 
   window.addEventListener('load', () => {
     navigator.serviceWorker
+      // Register all pages that are requested:
+      // .register('../sw_cached_site.js')
+      // Path for github pages production to work:
       .register('/Train-service-worker/sw_cached_site.js')
+      // Register only explicits pages:
       // .register('../sw_cached_pages.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
